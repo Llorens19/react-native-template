3ae016f79b22396e3f6cc7bf41665b649b1a6f93
Diego Llorens Soriano <llorenssorianodiego@gmail.com>
2025-06-01 10:36:22 +0200

    Correct errors

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
-export default () => {
+/* eslint-disable */
+module.exports = function (api) {
+  api.cache(true);
+
   return {
     presets: ['babel-preset-expo'],
-    plugins: [],
+    plugins: []
   };
-}
+};
