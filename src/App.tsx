c8438a87a43fddbc2b66c3624f3da984370c4c8c
Diego Llorens Soriano <llorenssorianodiego@gmail.com>
2025-06-01 14:23:05 +0200

    Add Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,11 @@
 import { Hola } from '@Hola';
 import { StatusBar } from 'expo-status-bar';
-import { JSX } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { View } from 'react-native';
 
-const App = (): JSX.Element => (
-  <View style={styles.container}>
+const App = () => (
+  <View className= "bg-gray-500">
     <Hola/>
     <StatusBar style="auto" />
   </View>
 );
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
 export default App;
